43c59d9b1354357498699bc13cd792ff4ed1af92
iOSBeginner <jenhausu@icloud.com>
2014-11-04 19:57:54 +0800

    Initialization

---
+++ tableView/tableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableView
+//
+//  Created by 蘇健豪1 on 2014/11/4.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tableView/tableView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tableView
+//
+//  Created by 蘇健豪1 on 2014/11/4.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

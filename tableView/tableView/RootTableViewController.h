1df23a7782afc0d7d1ef6e31541142c9f80cd3e8
iOSBeginner <jenhausu@icloud.com>
2014-11-04 20:01:38 +0800

    initialization

---
+++ tableView/tableView/RootTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootTableViewController.h
+//  tableView
+//
+//  Created by 蘇健豪1 on 2014/11/4.
+//  Copyright (c) 2014年 蘇健豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootTableViewController : UITableViewController
+
+@end

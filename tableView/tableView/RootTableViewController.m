f440bb69f8eb13139f8424f9c09dbac6bec3f9a0
iOSBeginner <jenhausu@icloud.com>
2014-11-04 20:08:12 +0800

    基本設定

--- tableView/tableView/RootTableViewController.m
+++ tableView/tableView/RootTableViewController.m
@@ -10,48 +10,42 @@
 
 @interface RootTableViewController ()
 
+@property (strong, nonatomic) NSArray *section1;
+@property (strong, nonatomic) NSArray *section2;
+@property (strong, nonatomic) NSArray *array;
+
 @end
 
 @implementation RootTableViewController
 
+@synthesize section1, section2, array;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    section1 = @[@"General", @"Privacy"];
+    section2 = @[@"iCloud", @"Map2", @"Safari", @"Photo & Camera", @"Game Center"];
+    array = @[section1, section2];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return array.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return [array[section] count];
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
     
-    // Configure the cell...
+    cell.textLabel.text = array[indexPath.section][indexPath.row];
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.

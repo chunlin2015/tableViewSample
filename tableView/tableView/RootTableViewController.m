759450f45b2b82972f56e714fd1cd93e7469698e
iOSBeginner <jenhausu@icloud.com>
2014-11-06 22:37:42 +0800

    儲存Switch Control的狀態

--- tableView/tableView/RootTableViewController.m
+++ tableView/tableView/RootTableViewController.m
@@ -51,6 +51,9 @@
             UISwitch *switchControl = [[UISwitch alloc] initWithFrame:CGRectMake(1.0, 1.0, 20.0, 30.0)];
             cell.accessoryView = switchControl;
             cell.selectionStyle = UITableViewCellSelectionStyleNone;
+            
+            NSUserDefaults *userPreferences = [NSUserDefaults standardUserDefaults];
+            switchControl.on = [userPreferences boolForKey:@"switchControl"];
         }
     }
     return cell;
